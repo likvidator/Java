8ec836ce911c218c714b0f0083e84bd4a998e5ad
Likvidator <max-kor4@yandex.ru>
2014-10-01 23:30:59 +0400

    Edit Unit

--- Program2/src/unit.java
+++ Program2/src/unit.java
@@ -1,13 +1,18 @@
 
 public class unit {
-    private float half;
-    private float power;
-    private float speed;
-    private float set;
+    public float half;
+    public float power;
+    public float speed;
+    public float set;
     public unit(float h, float p, float s ,float se){
         half=h;
         power=p;
         speed=s;
         set=se;
+    }
+
+    public static void main(String[] args){
+        People test = new People();
+        System.out.println(test.speed);
     }
 }
